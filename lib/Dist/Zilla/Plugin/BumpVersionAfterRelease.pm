7cc3d58361f8f168e94f3d51f9cf03f5e05b2d87
David Golden <dagolden@cpan.org>
2014-12-01 10:01:59 -0500

    After release: bump $VERSION and timestamp Changes

--- lib/Dist/Zilla/Plugin/BumpVersionAfterRelease.pm
+++ lib/Dist/Zilla/Plugin/BumpVersionAfterRelease.pm
@@ -5,7 +5,7 @@ use warnings;
 package Dist::Zilla::Plugin::BumpVersionAfterRelease;
 # ABSTRACT: Bump module versions after distribution release
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 
 use Moose;
 with(
--- lib/Dist/Zilla/Plugin/RewriteVersion.pm
+++ lib/Dist/Zilla/Plugin/RewriteVersion.pm
@@ -5,7 +5,7 @@ use warnings;
 package Dist::Zilla::Plugin::RewriteVersion;
 # ABSTRACT: Get and/or rewrite module versions to match distribution version
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 
 use Moose;
 with(
