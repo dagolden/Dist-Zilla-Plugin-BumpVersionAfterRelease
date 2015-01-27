d74fe79b42a5ada80148b9243fda2fcae9953ad0
David Golden <dagolden@cpan.org>
2015-01-27 06:48:03 -0500

    After release: bump $VERSION and timestamp Changes

--- lib/Dist/Zilla/Plugin/RewriteVersion.pm
+++ lib/Dist/Zilla/Plugin/RewriteVersion.pm
@@ -5,7 +5,7 @@ use warnings;
 package Dist::Zilla::Plugin::RewriteVersion;
 # ABSTRACT: Get and/or rewrite module versions to match distribution version
 
-our $VERSION = '0.009';
+our $VERSION = '0.010';
 
 use Moose;
 with(
